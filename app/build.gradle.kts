ec8c10e6dd1e0bd5b6929e639cfd5d3ca8517090
minax2036-beep <minax2036@gmail.com>
2026-09-19 22:34:37 +0900

    build.gradle.kts の更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
@@ -25,7 +24,6 @@ dependencies {
     val composeBom = platform("androidx.compose:compose-bom:2026.09.00")
 
     implementation(composeBom)
-    androidTestImplementation(composeBom)
 
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.compose.material3:material3")
