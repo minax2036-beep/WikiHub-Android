2f36ab95c790eeaa7087edcf007c19c6991cf3db
minax2036-beep <minax2036@gmail.com>
2026-09-20 09:15:47 +0900

    Create Screen.kt

---
+++ app/src/main/java/com/minax333/wikihub/navigation/Screen.kt
@@ -0,0 +1,11 @@
+package com.minax333.wikihub.navigation
+
+enum class Screen(
+    val title: String
+) {
+    HOME("ホーム"),
+    WIKI_LIST("Wiki一覧"),
+    RECENT_ARTICLES("最近の記事"),
+    STATISTICS("統計"),
+    SETTINGS("設定")
+}
