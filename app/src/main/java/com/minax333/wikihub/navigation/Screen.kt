6e2d4e60889584d0145b198b7aa0d68a01616b2d
minax2036-beep <minax2036@gmail.com>
2026-09-20 12:32:52 +0900

    Update Screen.kt

--- app/src/main/java/com/minax333/wikihub/navigation/Screen.kt
+++ app/src/main/java/com/minax333/wikihub/navigation/Screen.kt
@@ -7,5 +7,9 @@ enum class Screen(
     WIKI_LIST("Wiki一覧"),
     RECENT_ARTICLES("最近の記事"),
     STATISTICS("統計"),
-    SETTINGS("設定")
+    SETTINGS("設定"),
+
+    WIKI_CREATE("Wiki作成"),
+    WIKI_HOME("Wiki"),
+    WIKI_SETTINGS("Wiki設定")
 }
